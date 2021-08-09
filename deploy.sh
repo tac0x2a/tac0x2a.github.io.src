b17502eaabd1956f2b8620b0e993f9c4429363e3
TAC <tac@tac42.net>
2021-08-09 23:30:35 +0900

    Remove cd ../ code

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@
 set -e
 
 printf "\033[0;32mUpdate public directory...\033[0m\n"
-cd public
-git checkout -f master
-git pull origin master
-cd ../
+(
+	cd public
+	git checkout -f master
+	git pull origin master
+)
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
@@ -18,22 +19,24 @@ hugo -t even # if using a theme, replace with `hugo -t <YOURTHEME>`
 # mv public.git public/.git
 
 # Go To Public folder
-cd public
+(
+	cd public
 
-# Add changes to git.
-git add .
+	# Add changes to git.
+	git add .
 
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
+	# Commit changes.
+	msg="rebuilding site $(date)"
+	if [ -n "$*" ]; then
+		msg="$*"
+	fi
+	git commit -m "$msg"
 
-# Push source and build repos.
-git push origin master
+	# Push source and build repos.
+	git push origin master
+)
 
-cd ../
+
 git add public
 git commit -m "Update public submodule public - $msg"
 
